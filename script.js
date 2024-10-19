3ad6a0d2e91426739c7ea3d49eeee75a7f4fac87
surendrapatel18 <surupatel18991@gmail.com>
2024-10-19 18:52:39 +0530

    first commit

--- script.js
+++ script.js
@@ -1,36 +1,146 @@
+// Toggle-menu
 
-const menuToggle = document.getElementById('menuToggle');
-const mobileMenu = document.getElementById('mobileMenu');
+const menuToggle = document.getElementById("menuToggle");
+const mobileMenu = document.getElementById("mobileMenu");
 
-menuToggle.addEventListener('click', () => {
-  mobileMenu.classList.toggle('hidden');
+menuToggle.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
 });
 
+
 function closeMenu() {
-  mobileMenu.classList.add('hidden');
+  mobileMenu.classList.add("hidden");
 }
 
+// Smooth Scroll behavior
+
+document.querySelectorAll('.scroll-link').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const targetId = this.getAttribute('href').substring(1);
+    const targetSection = document.getElementById(targetId);
+
+    window.scrollTo({
+      top: targetSection.offsetTop,
+      behavior: 'smooth'
+    });
+
+    
+    closeMenu();
+  });
+});
 
 
-    var swiper = new Swiper(".mySwiper", {
-      spaceBetween: 30,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
+// Hero-Section Slider
+
+var swiper1 = new Swiper(".mySwiper1", {
+spaceBetween: 30,
+loop: true,
+pagination: {
+  el: ".swiper-pagination",
+  clickable: true,
+},
+autoplay: {
+  delay: 3000,
+  disableOnInteraction: false,
+},
+});
+
+
+
+
+//  Image-thumbnail video Play
+ 
+const thumbnail1 = document.getElementById('thumbnail1');
+const video1 = document.getElementById('video1');
+
+thumbnail1.addEventListener('click', () => {
+  thumbnail1.classList.add('hidden');  
+  video1.classList.remove('hidden');   
+  video1.play();                      
+});
+
+  
+
+// Brand-Logo slider
+
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 6,  
+    spaceBetween: 30,  
+    loop: true,  
+    scrollbar: {
+      el: ".swiper-scrollbar",
+      hide: false,  
+      draggable: true,  
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 2,  
+        spaceBetween: 10,  
       },
-    });
-    var swiper2 = new Swiper(".mySwiper2", {
-      spaceBetween: 30,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
+      640: {
+        slidesPerView: 3, 
+        spaceBetween: 15,
       },
-    });
-    var swiper3 = new Swiper(".mySwiper3", {
-      spaceBetween: 30,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
+      768: {
+        slidesPerView: 4,  
+        spaceBetween: 20,
       },
-    });
+      1024: {
+        slidesPerView: 5,  
+        spaceBetween: 25,
+      },
+      1280: {
+        slidesPerView: 6,  
+        spaceBetween: 30,
+      },
+    },
+  });
+
+
+// Card-Slider
+
+  var swiper3 = new Swiper(".mySwiper3", {
+    slidesPerView: 2, 
+    spaceBetween: 30, 
+    loop: true, 
+    pagination: {
+      el: ".swiper-pagination", 
+      clickable: true, 
+    },
+    autoplay: {
+      delay: 2000, 
+      disableOnInteraction: false, 
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1, 
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2, 
+        spaceBetween: 30, 
+      },
+    },
+  });
+  
+
+
+//  Image-thumbnail video Play
+ 
+const thumbnail2 = document.getElementById('thumbnail2');
+const video2 = document.getElementById('video2');
+
+thumbnail2.addEventListener('click', () => {
+  thumbnail2.classList.add('hidden');  
+  video2.classList.remove('hidden');   
+  video2.play();                      
+});
+
+
+
+
+// Updated Year 
+
+document.getElementById('year').textContent = new Date().getFullYear();
   
